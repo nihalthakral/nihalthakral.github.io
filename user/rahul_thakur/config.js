0e5fbb3a1640330c46c5a2c3d92f13602e79485f
nihalthakral <nihalthakral.trader@gmail.com>
2026-06-11 12:37:06 +0530

    Update config.js

--- user/rahul_thakur/config.js
+++ user/rahul_thakur/config.js
@@ -1,4 +1,4 @@
 const SOFTWARE = [
     { file: "easy_calculator/calculator.html",   name: "Easy Calculator"   },
-    { file: "gmail_checker/Gmail_Notification_Software.crx",   name: "Gmail Notification Software"   }
+    { file: "Gmail_Notification_Software.crx",   name: "Gmail Notification Software"   }
   ];
