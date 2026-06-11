09949f355c25481d9f1953917fdec5d916bccb8e
nihalthakral <nihalthakral.trader@gmail.com>
2026-06-11 10:32:03 +0530

    Update config.js

--- user/rahul_thakur/easy_calculator/config.js
+++ user/rahul_thakur/easy_calculator/config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
 
   // Client ka naam (header aur install screen mein dikhega)
-  clientName: "Template",
+  clientName: "Rahul Thakur",
 
   // Header emoji (dukaan ki type ke hisaab se choose karo)
   headerEmoji: "🛍️",
 
   // Free trial khatam hone ki date — format: "YYYY-MM-DD"
-  expiryDate: "2999-10-14",
+  expiryDate: "2026-7-11",
 
   // Tracker naam — spaces ki jagah _ use karo
-  trackerName: "tracker",
+  trackerName: "Rahul_Thakur_Easy_Calculator",
 
   // Services / Products list
   // unit examples: "page", "meter", "piece", "kg", "item", "sheet"
   // Minimum 1, Recommended = 4, Maximum = 10 
   services: [
-    { id: "1", emoji: "🛒", name: "Product 1", rate: 3,  unit: "page"  },
-    { id: "2", emoji: "🛒", name: "Product 2", rate: 5,  unit: "meter"  },
-    { id: "3", emoji: "🛒", name: "Product 3", rate: 10, unit: "piece"  },
-    { id: "4", emoji: "🛒", name: "Product 4", rate: 10, unit: "kg"  }
+    { id: "1", emoji: "📄", name: "Photo Copy", rate: 3,  unit: "page"  },
+    { id: "2", emoji: "🖨️", name: "Saada Printout", rate: 5,  unit: "page"  },
+    { id: "3", emoji: "🌈", name: "Colored Printout", rate: 10, unit: "page"  },
+    { id: "4", emoji: "🎨", name: "Colored Photocopy", rate: 10, unit: "page"  }
   ]
 
 };
