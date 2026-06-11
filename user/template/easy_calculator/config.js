04e5556b4f84ee7eab5a0d19adebbcf4a412bd49
nihalthakral <nihalthakral.trader@gmail.com>
2026-06-11 10:13:38 +0530

    Update config.js

--- user/template/easy_calculator/config.js
+++ user/template/easy_calculator/config.js
@@ -14,17 +14,12 @@ const CONFIG = {
 
   // Services / Products list
   // unit examples: "page", "meter", "piece", "kg", "item", "sheet"
+  // Minimum 1, Recommended = 4, Maximum = 10 
   services: [
     { id: "1", emoji: "🛒", name: "Product 1", rate: 3,  unit: "page"  },
-    { id: "2", emoji: "🛒", name: "Product 2", rate: 5,  unit: "kg"  },
-    { id: "3", emoji: "🛒", name: "Product 3", rate: 10, unit: "gram"  },
-    { id: "4", emoji: "🛒", name: "Product 4", rate: 10, unit: "leter"  },
-    { id: "5", emoji: "🛒", name: "Product 5", rate: 3,  unit: "page"  },
-    { id: "6", emoji: "🛒", name: "Product 6", rate: 5,  unit: "page"  },
-    { id: "7", emoji: "🛒", name: "Product 7", rate: 10, unit: "page"  },
-    { id: "8", emoji: "🛒", name: "Product 8", rate: 10, unit: "page"  },
-    { id: "9", emoji: "🛒", name: "Product 9", rate: 10, unit: "page"  },
-    { id: "10", emoji: "🛒", name: "Product 10", rate: 10, unit: "page"  }
+    { id: "2", emoji: "🛒", name: "Product 2", rate: 5,  unit: "meter"  },
+    { id: "3", emoji: "🛒", name: "Product 3", rate: 10, unit: "piece"  },
+    { id: "4", emoji: "🛒", name: "Product 4", rate: 10, unit: "kg"  }
   ]
 
 };
