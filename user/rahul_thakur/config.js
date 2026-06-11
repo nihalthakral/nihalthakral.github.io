3ee18c62e2096bc53049fed75de2445284798984
nihalthakral <nihalthakral.trader@gmail.com>
2026-06-11 11:20:37 +0530

    Update config.js

--- user/rahul_thakur/config.js
+++ user/rahul_thakur/config.js
@@ -3,3 +3,9 @@ const SOFTWARE = [
     { file: "gmail_checker/Gmail_Notification_Software.crx",   name: "Gmail Notification Software"   }
   ];
 
+(function() {
+  var name = "Rahul_Thakur_Download_Page";
+  var img  = new Image();
+  img.src  = "https://visitor-tracker.nihalthakral-trader.workers.dev/track?name="
+             + encodeURIComponent(name) + "&t=" + Date.now();
+})();
