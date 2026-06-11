5feace6a64eafe1cb80bd53da67af4e376de855e
nihalthakral <nihalthakral.trader@gmail.com>
2026-06-11 09:05:36 +0530

    Update config.js

--- user/template/easy_calculator/config.js
+++ user/template/easy_calculator/config.js
@@ -19,10 +19,6 @@ const CONFIG = {
     { id: "2", emoji: "🖨️", name: "Saada Printout",    rate: 5,  unit: "page"  },
     { id: "3", emoji: "🌈", name: "Colored Printout",  rate: 10, unit: "page"  },
     { id: "4", emoji: "🎨", name: "Colored Photocopy", rate: 10, unit: "page"  },
-    { id: "5", emoji: "📄", name: "Photo Copy",        rate: 3,  unit: "page"  },
-    { id: "6", emoji: "🖨️", name: "Saada Printout",    rate: 5,  unit: "page"  },
-    { id: "7", emoji: "🌈", name: "Colored Printout",  rate: 10, unit: "page"  },
-    { id: "8", emoji: "🎨", name: "Colored Photocopy", rate: 10, unit: "page"  },
   ]
 
 };
