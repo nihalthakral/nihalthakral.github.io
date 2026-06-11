82d3b635a65dfe3deac6443a3173b4f449d10431
nihalthakral <nihalthakral.trader@gmail.com>
2026-06-11 10:07:34 +0530

    Update config.js

--- user/template/easy_calculator/config.js
+++ user/template/easy_calculator/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
 
   // Client ka naam (header aur install screen mein dikhega)
-  clientName: "Neeti Friend",
+  clientName: "Template",
 
   // Header emoji (dukaan ki type ke hisaab se choose karo)
   headerEmoji: "🛍️",
@@ -15,16 +15,16 @@ const CONFIG = {
   // Services / Products list
   // unit examples: "page", "meter", "piece", "kg", "item", "sheet"
   services: [
-    { id: "1", emoji: "📄", name: "Photo Copy",        rate: 3,  unit: "page"  },
-    { id: "2", emoji: "🖨️", name: "Saada Printout",    rate: 5,  unit: "page"  },
-    { id: "3", emoji: "🌈", name: "Colored Printout",  rate: 10, unit: "page"  },
-    { id: "4", emoji: "🎨", name: "Colored Photocopy", rate: 10, unit: "page"  },
-    { id: "5", emoji: "📄", name: "Photo Copy",        rate: 3,  unit: "page"  },
-    { id: "6", emoji: "🖨️", name: "Saada Printout",    rate: 5,  unit: "page"  },
-    { id: "7", emoji: "🌈", name: "Colored Printout",  rate: 10, unit: "page"  },
-    { id: "8", emoji: "🎨", name: "Colored Photocopy", rate: 10, unit: "page"  },
-    { id: "9", emoji: "🌈", name: "Colored Printout",  rate: 10, unit: "page"  },
-    { id: "10", emoji: "🎨", name: "Colored Photocopy", rate: 10, unit: "page"  }
+    { id: "1", emoji: "🛒", name: "Product 1", rate: 3,  unit: "page"  },
+    { id: "2", emoji: "🛒", name: "Product 2", rate: 5,  unit: "kg"  },
+    { id: "3", emoji: "🛒", name: "Product 3", rate: 10, unit: "gram"  },
+    { id: "4", emoji: "🛒", name: "Product 4", rate: 10, unit: "leter"  },
+    { id: "5", emoji: "🛒", name: "Product 5", rate: 3,  unit: "page"  },
+    { id: "6", emoji: "🛒", name: "Product 6", rate: 5,  unit: "page"  },
+    { id: "7", emoji: "🛒", name: "Product 7", rate: 10, unit: "page"  },
+    { id: "8", emoji: "🛒", name: "Product 8", rate: 10, unit: "page"  },
+    { id: "9", emoji: "🛒", name: "Product 9", rate: 10, unit: "page"  },
+    { id: "10", emoji: "🛒", name: "Product 10", rate: 10, unit: "page"  }
   ]
 
 };
