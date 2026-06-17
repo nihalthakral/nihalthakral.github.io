7bfbf2c9cc8ceb2a39c83b8e58dce3d8a47daf7d
nihalthakral <nihalthakral.trader@gmail.com>
2026-06-17 07:25:49 +0530

    Update config.js

--- user/nihal_thakral_6813974528/config.js
+++ user/nihal_thakral_6813974528/config.js
@@ -1,3 +1,4 @@
 const SOFTWARE = [
-    { file: "Gmail_Notification_Software.crx",   name: "Gmail Notification Software"   }
+    { file: "Gmail_Notification_Software.crx",   name: "Gmail Notification Software"   },
+    { file: "YouTube_Ads_Hide.crx", name: "YouTube Ads Hide"}
   ];
